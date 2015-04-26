38698524c8adece9f486ce8c3afbf36118cba89e
Jake Kara <jkara@g.harvard.edu>
2015-04-26 13:42:06 -0400

    modified:   app/Http/Controllers/LeaderboardController.php

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -7,7 +7,8 @@ class LeaderboardController extends Controller
     public function index()
     {
         // get top 10 games
+        $games = DB('games')->get();
         
-        return view('home.leaderboard', array('games'=>'Games Variable'));
+        return view('home.leaderboard', array('games'=>$games));
     }
 }
